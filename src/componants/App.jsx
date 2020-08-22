a88bc7b9ddd366f59269e3abf79ac481e14935eb
Souma <souma.hitech@gmail.com>
2020-08-22 23:57:31 +0530

    complete add functionality.

--- src/componants/App.jsx
+++ src/componants/App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes"
+// import notes from "../notes"
 import InputArea from "./InputArea";
 
 
 function App() {
+
+    const [notes, setNotes] = useState([{
+        title:"This is title",
+        content:"This is content"
+    }]);
+
+    function addNotes(note) {
+        console.log(note);
+        setNotes((previousNotes)=>{
+            return [...previousNotes,note];
+        })
+    }
+
     return (
         <div>
             <Header/>
-            <InputArea/>
+            <InputArea
+                onAdd = {addNotes}
+            />
             {notes.map((note) => (
                 <Note
                     key = {note.key}
--- src/componants/InputArea.jsx
+++ src/componants/InputArea.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputArea() {
+function InputArea(props) {
+
+    const [note, updateNote] = useState({
+        title:"",
+        content:""
+    });
+
+
+    function inputHandler(event) {
+        // use object destructure 
+        // N:B event.target is a object
+        const {value,name} = event.target;
+        updateNote((previousValue)=>{
+            return {
+                // use sprade operator basically we add previous object 
+                // and then update property of that object
+                ...previousValue,
+                [name]:value
+            }
+        })
+    }
+  
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input 
+            name="title" 
+            placeholder="Title"
+            onChange={inputHandler}  
+            value = {note.title}
+            />
+        <textarea 
+            name="content" 
+            placeholder="Take a note..." 
+            rows="3"
+            onChange={inputHandler} 
+            value={note.content}
+            />
+        <button type ="submit" onClick={(event)=>{
+            props.onAdd(note);
+            updateNote({
+                title:"",
+                content:""
+            })
+            event.preventDefault();
+        }}>Add</button>
       </form>
     </div>
   );
